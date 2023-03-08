e513759f5fd7a5c2948326bdc43343df07a885d8
HyunKyu <yunhk2001@gmail.com>
2023-03-08 22:14:35 +0900

    Thread Pool 적용

--- src/main/java/com/example/oop/CustomWebApplicationServer.java
+++ src/main/java/com/example/oop/CustomWebApplicationServer.java
@@ -23,8 +23,8 @@ import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
-
-    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+    
+    private final ExecutorService executorsService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -41,37 +41,9 @@ public class CustomWebApplicationServer {
 
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!");
-
-                /*
-                 * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
-                */
                 
-                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-                	BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                	DataOutputStream dos = new DataOutputStream(out);
-                	
-//                	String line;
-//                	while((line = br.readLine()) != "") {
-//                		System.out.println(line);
-//                	}
-                	
-                	HttpRequest httpRequest = new HttpRequest(br);
-                	
-                	if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                		QueryStrings queryStrings = httpRequest.getQueryStrings();
-                		
-                		int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                		String operator = queryStrings.getValue("operator");
-                		int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-                		
-                		int result = Calculator.calculate(new PositiveNumber(operand1), operator,new PositiveNumber(operand2));
-                		byte[] body = String.valueOf(result).getBytes();
-                		
-                		HttpResponse response = new HttpResponse(dos);
-                		response.response200Header("application/json", body.length);
-                		response.responseBody(body);
-                	}
-                }
+                // Thread Pool 적용, 안정적인 서비스 가능
+                executorsService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
